6531f97159e46ace9aed5ab64ef0980f74adee4d
subarnoturja <subarnoturja15@gmail.com>
2024-08-22 11:51:57 +0600

    setup done

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Home from "../components/Home/Home";
+import MainLayout from "../MainLayout/MainLayout";
+
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>,
+            },
+        ]
+    }
+]);
