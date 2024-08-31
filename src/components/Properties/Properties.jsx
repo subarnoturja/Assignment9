cb648467f49c2d77e913eaec7fc9d354b25675fb
subarnoturja <subarnoturja15@gmail.com>
2024-08-31 20:56:46 +0600

    estate section added

--- src/components/Properties/Properties.jsx
+++ src/components/Properties/Properties.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import PropertyCard from '../PropertyCard/PropertyCard';
 
 const Properties = () => {
+    const properties = useLoaderData()
     return (
-        <div className='lg:w-[90%] m-auto lg:px-20 px-6 py-20 w-full flex flex-col justify-center items-center gap-10'>
-            <div className='flex flex-col justify-center items-start gap-4'> 
+        <div>
+            <div className='flex flex-col justify-center items-center text-center gap-4 mb-20'> 
                 <h1 data-aos='zoom-in' className='text-red-500'>PROPERTIES</h1>
-                <h1 data-aos='zoom-in' className='text-black text-4xl font-semibold'>Explore the latest</h1>
+                <h1 data-aos='zoom-in' className='text-black text-4xl font-semibold'>Explore The Latest <br /> Residential Properties</h1>
             </div>
-            
+            <div data-aos='fade-up' className='lg:px-24 lg:ml-14 grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+                {
+                    properties.map(property => (
+                        <PropertyCard 
+                            key={property.id}
+                            property={property}
+                        ></PropertyCard>
+                    ))
+                }
+            </div>
         </div>
     );
 };
--- src/components/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -1,9 +1,108 @@
 import React from "react";
+import { FaBangladeshiTakaSign, FaBath, FaBed } from "react-icons/fa6";
+import { PiBuildingApartmentLight } from "react-icons/pi";
+import { SlLocationPin } from "react-icons/sl";
 
-const PropertyCard = () => {
+const PropertyCard = ({ property }) => {
+  const {
+    id,
+    estate_title,
+    segment_name,
+    description,
+    price,
+    status,
+    area,
+    location,
+    facilities,
+    apartment_img,
+    total_bedroom,
+    total_washroom
+  } = property;
+
   return (
-    <div>
-        
+    <div className="max-w-xl container border-2 p-4 mb-6 rounded-xl shadow-lg transform hover:scale-105">
+      <div className="flex justify-between pb-4 border-bottom">
+        <div className="flex items-center">
+          <a
+            rel="noopener noreferrer"
+            className="mb-0 capitalize px-4 border rounded-full bg-orange-600 text-white"
+          >
+            {segment_name}
+          </a>
+        </div>
+        <a
+          rel="noopener noreferrer"
+          href="#"
+          className="mb-0 capitalize px-4 border rounded-full bg-green-600 text-white"
+        >
+          {status}
+        </a>
+      </div>
+      <div className="space-y-4">
+        <div className="space-y-2">
+          <img
+            src={apartment_img}
+            alt=""
+            className="block object-cover object-center w-full rounded-md h-60"
+          />
+          <div className="flex gap-2 items-center text-md">
+          <SlLocationPin />
+            <span> {location}</span>
+          </div>
+        </div>
+        {/* Title & Description */}
+        <div className="space-y-2">
+          <a rel="noopener noreferrer" href="#" className="block">
+            <h3 className="text-xl font-semibold dark:text-default-600">
+              {estate_title}
+            </h3>
+          </a>
+          <p className="leading-snug dark:text-gray-600">
+            {description.substring(0, 50)}...
+          </p>
+        </div>
+        {/* Bedroom & Washroom */}
+        <div className="flex gap-8">
+          {/* Bedroom */}
+          <div className="flex gap-2 items-center">
+            <FaBed className="text-2xl" />
+            {total_bedroom}
+          </div>
+          {/* Washroom */}
+          <div className="flex gap-2 items-center">
+            <FaBath className="text-2xl" />
+            {total_washroom}
+          </div>
+        </div>
+        {/* Facilities */}
+        <div className="flex gap-4">
+          <div className="border-2 rounded-full px-2.5 py-1 bg-[#23BE0A0D] text-[#23BE0A]">
+            <h4>{facilities[0]}</h4>
+          </div>
+          <div className="border-2 rounded-full px-2.5 py-1 bg-[#23BE0A0D] text-[#23BE0A]">
+            <h4>{facilities[1]}</h4>
+          </div>
+          <div className="border-2 rounded-full px-2.5 py-1 bg-[#23BE0A0D] text-[#23BE0A]">
+            <h4>{facilities[2]}</h4>
+          </div>
+        </div>
+        {/* Area & Price */}
+        <div className="flex justify-between">
+          {/* Price */}
+          <div className="flex gap-2 items-center">
+          <FaBangladeshiTakaSign className="text-xl"/>
+          {price}
+          </div>
+          {/* Area */}
+          <div className="flex gap-2 items-center">
+          <PiBuildingApartmentLight className="text-xl"/>
+          {area} sq ft
+          </div>
+        </div>
+        <button className="btn btn-success text-white w-full">
+          View Property
+        </button>
+      </div>
     </div>
   );
 };
